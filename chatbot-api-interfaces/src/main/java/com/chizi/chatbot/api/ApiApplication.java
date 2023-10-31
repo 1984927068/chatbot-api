e9218065729bc7b7ac9c1d11e0734ee4e4c937fd
1984927068 <1984927068@qq.com>
2023-11-01 00:12:51 +0800

    新建项目

---
+++ chatbot-api-interfaces/src/main/java/com/chizi/chatbot/api/ApiApplication.java
@@ -0,0 +1,18 @@
+package com.chizi.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author chizi
+ * @Description 启动入口
+ * @Date 2023/10/31 23:40
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
